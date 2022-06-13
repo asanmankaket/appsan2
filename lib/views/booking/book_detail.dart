f29fe104be9c7eedff03f1431c922178b75ab731
Nutcras <nazreebang@gmail.com>
2022-06-13 21:54:06 +0700

    ver13-7-65nut

---
+++ lib/views/booking/book_detail.dart
@@ -0,0 +1,266 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class Bookdetail extends StatefulWidget {
+  const Bookdetail({Key? key, required this.data}) : super(key: key);
+
+  final dynamic data;
+  @override
+  State<Bookdetail> createState() => _BookdetailState();
+}
+
+class _BookdetailState extends State<Bookdetail> {
+  get children => null;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ข้อมูลลูกค้า'),
+        backgroundColor: const Color.fromARGB(255, 160, 42, 207),
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 40,
+              ),
+              const Center(
+                //รูปavatar
+                child: CircleAvatar(
+                  radius: 70,
+                  // backgroundImage: ('${widget.data['image']}'),
+                  backgroundColor: Color.fromARGB(255, 45, 134, 156),
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              const Padding(
+                padding: EdgeInsets.only(left: 50),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'เวลานัด',
+                    style:
+                        TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(20.0),
+                child: Container(
+                  color: const Color.fromARGB(255, 150, 217, 234),
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        'วันที่ : ' +
+                            DateFormat('dd-mm-yy KK:MM').format(
+                                DateTime.parse('${widget.data['start_time']}')),
+                        style: const TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 7,
+                      ),
+                      Text(
+                        'เวลาตั้งเเต่ :  ' +
+                            DateFormat('dd-mm-yy KK:MM').format(
+                                DateTime.parse('${widget.data['end_time']}')),
+                        style: const TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              ClipRRect(
+                // borderRadius: BorderRadius.circular(10.0),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 50),
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: const Text(
+                          'อายุ 68 ปี',
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.normal),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    // Text(
+                    //   'ชื่อ ${widget.data['fname']} ${widget.data['lname']}',
+                    //   style: TextStyle(fontSize: 18),
+                    // ),
+                    const Padding(
+                      padding: EdgeInsets.only(left: 50),
+                      child: const Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          'โรคประจำตัว',
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.only(left: 70),
+                      child: const Align(
+                        alignment: Alignment.centerLeft,
+                        child: const Text(
+                          '- โรคหัวใจ',
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.normal),
+                        ),
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.only(left: 70),
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          '- โรคเบาหวาน',
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.normal),
+                        ),
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.only(left: 70),
+                      child: const Align(
+                        alignment: Alignment.centerLeft,
+                        child: const Text(
+                          '- โรคความดัน',
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.normal),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const Padding(
+                      padding: const EdgeInsets.only(left: 50),
+                      child: const Align(
+                        alignment: Alignment.centerLeft,
+                        child: const Text(
+                          'ลักษณะผู้ป่วย',
+                          style: const TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.only(left: 70),
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          '- ผู้ป่วยติดเตียง',
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.normal),
+                        ),
+                      ),
+                    ),
+                    // Text('ราคาต่อชั่วโมง ${widget.data['rate']}',
+                    //     style: TextStyle(fontSize: 18)),
+                    // Text('${widget.data['type']}',
+                    //     style: TextStyle(fontSize: 18)),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Row(
+                children: [
+                  Row(
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.only(left: 40),
+                        child: SizedBox(
+                          width: 40,
+                        ),
+                      ),
+                      // ignore: prefer_const_constructors
+                      Icon(
+                        Icons.call,
+                        color: Colors.blue,
+                        size: 30,
+                      ),
+                      Text(
+                        '   ${widget.data['phone']}',
+                        style: const TextStyle(fontSize: 18),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: [
+                  Row(
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.only(left: 40),
+                        child: SizedBox(
+                          width: 40,
+                        ),
+                      ),
+                      const Icon(
+                        Icons.map,
+                        color: Colors.blue,
+                      ),
+                      Text(
+                        '''   ${widget.data['adr1']} ${widget.data['adr2']} 
+    ${widget.data['city']}''',
+                        style: const TextStyle(fontSize: 18),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              // TextButton(
+              //   style: TextButton.styleFrom(
+              //     padding: const EdgeInsets.fromLTRB(70, 15, 70, 15),
+              //     shape: RoundedRectangleBorder(
+              //       borderRadius: BorderRadius.circular(10.0),
+              //     ),
+              //     backgroundColor: Color.fromARGB(255, 232, 92, 22),
+              //     primary: Color.fromARGB(255, 255, 255, 255),
+              //     textStyle: const TextStyle(fontSize: 20),
+              //   ),
+              //   onPressed: () {},
+              //   child: const Text('ยกเลิกนัด'),
+              // ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/views/booking/booking.dart
+++ lib/views/booking/booking.dart
@@ -1,12 +1,8 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_appcare/views/book_detail.dart';
-import 'package:http/http.dart' as http;
+import 'package:creative/views/booking/book_detail.dart';
 import 'package:intl/intl.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import '../models/sidemenu.dart';
-import 'mainpage.dart';
+import '../../configs/api.dart';
+import '../../models/sidemenu.dart';
 
 class Booking extends StatefulWidget {
   const Booking({Key? key}) : super(key: key);
@@ -16,19 +12,15 @@ class Booking extends StatefulWidget {
 }
 
 class _BookingState extends State<Booking> {
-  @override
   dynamic data;
-
+  @override
   void initState() {
     super.initState();
     startApi();
   }
 
   startApi() async {
-    final prefs =
-        await SharedPreferences.getInstance(); //เพิ่มตัวแชร์จากหน้าlogin
-    int? idUser = prefs.getInt('idm');
-    dynamic item = await Getdata(idUser); //ส่งค่าไปยัง getdataหรือตัวรับapi
+    dynamic item = await getdata(72); //ส่งค่าไปยัง getdataหรือตัวรับapi
     setState(() {
       data = item;
     });
@@ -40,7 +32,7 @@ class _BookingState extends State<Booking> {
       backgroundColor: Colors.white,
       appBar: AppBar(
         title: const Text('นัดหมาย'),
-        backgroundColor: Color.fromARGB(255, 160, 42, 207),
+        backgroundColor: const Color.fromARGB(255, 160, 42, 207),
         // actions: <Widget>[
         //   IconButton(
         //     icon: Icon(
@@ -76,16 +68,17 @@ class _BookingState extends State<Booking> {
                       context,
                       MaterialPageRoute<void>(
                           builder: (BuildContext context) =>
-                              bookdetail(data: data[i])));
+                              Bookdetail(data: data[i])));
                 },
                 child: Card(
                   elevation: 10,
-                  color: Color.fromARGB(255, 205, 94, 249),
-                  shadowColor: Color.fromARGB(255, 10, 91, 111),
+                  color: const Color.fromARGB(255, 205, 94, 249),
+                  shadowColor: const Color.fromARGB(255, 10, 91, 111),
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(27),
                     side: BorderSide(
-                      color: Color.fromARGB(255, 0, 0, 0).withOpacity(0.5),
+                      color:
+                          const Color.fromARGB(255, 0, 0, 0).withOpacity(0.5),
                       width: 1,
                     ),
                   ),
@@ -150,29 +143,7 @@ class _BookingState extends State<Booking> {
           ),
         ),
       ),
-      drawer: SideMenu(), //หน้าปุ่มsidemenu
+      drawer: const SideMenu(), //หน้าปุ่มsidemenu
     );
   }
-}
-
-Future<dynamic> Getdata(dynamic idUser) async {
-  Uri url = Uri.parse(
-      'http://206.189.92.71:3200/api/booking/men/72/$idUser '); //รับค่ามาจากiduser หรือตัวที่แชร์มาจากหน้าlogin ส่งไปยังurlเพื่อเช็คว่าคนนี้มีนัดหมายใครบ้าง
-  // Uri url = Uri.parse(
-  //     'http://192.168.1.9:3200/api/booking/cust/$idUser'); //รับค่ามาจากiduser หรือตัวที่แชร์มาจากหน้าlogin ส่งไปยังurlเพื่อเช็คว่าคนนี้มีนัดหมายใครบ้าง
-  return await http
-      .get(
-    url,
-  )
-      .then((req) async {
-    print(req.statusCode);
-    if (req.statusCode == 200) {
-      print(req.body);
-      var data = jsonDecode(req.body);
-      return data;
-    } else {
-      print('error');
-      return null;
-    }
-  });
 }
