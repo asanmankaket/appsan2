7d98dcc8fe646e1021867a77578a69a46b5d3365
nutcras <83870987+nutcras@users.noreply.github.com>
2022-06-28 18:52:20 +0700

    ver28-06-65

--- lib/views/editprofile/setting/Repassword.dart
+++ lib/views/editprofile/setting/Repassword.dart
@@ -16,10 +16,10 @@ class _RepasswordState extends State<Repassword> {
   @override
   void initState() {
     super.initState();
-    startApi();
+    startApi1();
   }
 
-  startApi() async {
+  startApi1() async {
     dynamic item = await getProfile(); //ส่งค่าไปยัง getdataหรือตัวรับapi
     setState(() {
       data = item;
