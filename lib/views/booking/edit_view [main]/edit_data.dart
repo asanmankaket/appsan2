dce8e8b9f582621a932dcaeee1ace6280afe9ed7
asanmankaket <624234014@parichat.skru.ac.th>
2022-08-12 23:42:50 +0700

    san

--- lib/views/booking/edit_view [main]/edit_data.dart
+++ lib/views/booking/edit_view [main]/edit_data.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
 class Editdata extends StatefulWidget {
@@ -21,15 +23,19 @@ class _EditdataState extends State<Editdata> {
         child: Center(
           child: Column(
             children: [
-             Text('อัตราค่าบริการตามค่าเฉลี่ย'),
-             SizedBox(height: 20,),
-             Text('กำหนดค่าบริการ'),
-             SizedBox(height: 20,),
-             Text('กำหนดวันเวลาที่ให้บริการ....'),
+              Text('อัตราค่าบริการตามค่าเฉลี่ย'),
+              SizedBox(
+                height: 20,
+              ),
+              Text('กำหนดค่าบริการ'),
+              SizedBox(
+                height: 20,
+              ),
+              Text('กำหนดวันเวลาที่ให้บริการ....'),
             ],
           ),
         ),
       ),
-      );
+    );
   }
 }
