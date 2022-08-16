6e99c5c9acc9d66273dcd9c5d269e46ff505df07
Nutcras <nazreebang@gmail.com>
2022-08-16 20:10:45 +0700

    เพิ่มเติม

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyCnl5zRew6L00sZg2vu_JJ_BlCJpVvrTOs")
+    GMSServices.provideAPIKey("AIzaSyCl-NG8BnTUQX1T3Zw0FCBRR1qdXWt9KKs")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
