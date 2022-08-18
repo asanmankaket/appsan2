d890796ac84f4c6dfb1174aef6006cb8a06ce608
Nutcras <nazreebang@gmail.com>
2022-08-19 01:26:55 +0700

    แก้ตรงipนิดหน่อย

--- lib/configs/api.dart
+++ lib/configs/api.dart
@@ -185,7 +185,7 @@ Future sendDataProfile1(oldpassword, password, context) async {
   final prefs =
       await SharedPreferences.getInstance(); //เพิ่มตัวแชร์จากหน้าlogin
   int? idUser = prefs.getInt('idm');
-  Uri url = Uri.parse('http://192.168.1.143:3200/api/mentor/p1/$idUser');
+  Uri url = Uri.parse('http://206.189.92.71:3200/api/mentor/p1/$idUser');
   http
       .put(
     url,
