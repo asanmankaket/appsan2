73bc0a358b48000cdda112f1d25c90dc82f0fecc
nutcras <83870987+nutcras@users.noreply.github.com>
2022-10-31 21:50:26 +0700

    ลบบ่อยางออกจากสารบัญตำบล

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   await dotenv.load(fileName: ".env");
-  runApp(const MyApp());
+    runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
